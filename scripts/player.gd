7fd97d5b559fc1eba6a56309d10aa814e7d1e250
Tray Simpson <tdsimpson123@gmail.com>
2024-07-14 11:44:59 -0600

    Feat: Start basic goblin scene, fix sprite flip

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+@onready var animated_sprite = $AnimatedSprite2D
 @export var player_speed: int = 1
 
 
@@ -11,5 +11,9 @@ func _physics_process(delta):
 		int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")),
 		int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	).normalized() * (player_speed * delta)
+	if (velocity.x > 0):
+		animated_sprite.flip_h = false
+	elif (velocity.x < 0):
+		animated_sprite.flip_h = true
 
 	position += velocity
