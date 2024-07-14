3b25ebabbeb5758e86979757ce3a0f748305cbf0
Tray Simpson <tdsimpson123@gmail.com>
2024-07-14 16:58:13 -0600

    Adds progress bar for projectile spawner

--- BoltSpawner.gd
+++ BoltSpawner.gd
@@ -4,7 +4,15 @@ signal bolt_spawned(BoltAttack, int)
 
 @export var bolt_attack_prefab: PackedScene = null
 @export var bolt_count: int = 1
-	
+@export var cooldown_time: int = 1
+
+
+func _ready():
+	$CooldownTimer.wait_time = cooldown_time
+	$ProgressBar.max_value = cooldown_time
+
+func _process(delta):
+	$ProgressBar.value = cooldown_time - $CooldownTimer.time_left
 
 func _on_cooldown_timer_timeout():
 	for index in range(bolt_count):
