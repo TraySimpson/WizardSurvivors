1429d8f03f21340d5c86ac844b8e42724ce42204
Tray Simpson <tdsimpson123@gmail.com>
2024-07-14 17:43:31 -0600

    Adds goblin spawns, bug with health bar color is global

--- main.gd
+++ main.gd
@@ -2,13 +2,22 @@ extends Node
 
 
 const goblin_prefab = preload("res://goblin.tscn")
+var spawn_pos: PathFollow2D = null
 
 func _ready():
 	$Goblin.setFollowTarget($Player)
+	spawn_pos = $GoblinSpawns/GoblinSpawnPos
 
 
 func spawnGoblins():
-	pass
+	spawnGoblin()
+	
+func spawnGoblin():
+	spawn_pos.progress_ratio = randf()
+	var goblin = goblin_prefab.instantiate()
+	goblin.setFollowTarget($Player)
+	add_child(goblin)
+	goblin.position = spawn_pos.position
 
 func _on_goblin_spawn_cooldown_timeout():
 	spawnGoblins()
